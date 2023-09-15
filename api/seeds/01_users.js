bbaa5e0e686cd6b6d03bfe05f50494db2eada3e2
Connor Smith <cmsmith1217@gmail.com>
2023-09-15 14:41:35 -0500

    seedy boiz

--- api/seeds/01_users.js
+++ api/seeds/01_users.js
@@ -11,6 +11,6 @@ exports.seed = async function(knex) {
    {first_name: 'Jamel', last_name: 'Sanders', username: 'MelSandz', password: 'P@ssw0rd', profile_pic: 'pic url', user_summary: '', is_supracoder: true},
    {first_name: 'Bodie', last_name: 'Stodie', username: 'GoodBoy22', password: 'P@ssw0rd', profile_pic: 'pic url', user_summary: '', is_supracoder: false},
    {first_name: 'Remi', last_name: 'Stemi', username: 'PrettyGirl23', password: 'P@ssw0rd', profile_pic: 'pic url', user_summary: '', is_supracoder: false},
-   {first_name: 'Harley', last_name: 'Quinn', username: 'harls93', password: 'ives', profile_pic: 'url', user_summary: "It's good to be bad", is_supracoder: true}
+   {first_name: 'Harley', last_name: 'Quinn', username: 'harls93', password: 'ives', profile_pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNQuJiNP6h5zcgT2AGI6wtb5J7GhDBDB9htiJaotujsKLHEIUKUc-illIDxZjRTwTr9Nw&usqp=CAU', user_summary: "It's good to be bad", is_supracoder: true}
  ]);
 };
