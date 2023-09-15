6fba16ae633484a602b7511c9773a003c731750d
Connor Smith <cmsmith1217@gmail.com>
2023-09-15 13:58:24 -0500

    migration tables

---
+++ api/migrations/02_20230914231525_create_projects.js
@@ -0,0 +1,32 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('project_table', table => {
+    table.increments('id');
+    table.string('name');
+    table.string('problem_statement');
+    table.integer('submitter_id');
+    table.foreign('submitter_id').references('user_table.id');
+    table.boolean('is_approved');
+    table.boolean('is_accepted');
+    table.integer('accepted_by_id');
+    table.foreign('accepted_by_id').references('user_table.id');
+    table.boolean('is_completed');
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('project_table', table => {
+    table.dropForeign('submitter_id')
+    table.dropForeign('accepted_by_id')
+  })
+  .then(function() {
+    return knex.schema.dropTableIfExists('project_table')
+  })
+};
