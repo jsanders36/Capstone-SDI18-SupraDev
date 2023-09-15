029e8e374543b81682463236e51ec97ffec360d2
rudy <YaBoiWR@hotmail.com>
2023-09-15 14:22:11 -0600

    added project CRUD functionaility, a few bugs with the foreign keys which need to be fixed, none forign key items work as intended.

--- api/express.js
+++ api/express.js
@@ -57,7 +57,71 @@ app.delete('/users/:id', (req, res) => {
     .then(res.send('user deleted'))
 })
 
+app.get('/projects', (req, res) => {
+  knex('project_table')
+    .select('*')
+    .then((project) => {res.send(project)});
+})
 
+app.get('/projects/:id', (req, res) => {
+  knex('project_table')
+    .select('*')
+    .where({ id: req.params.id })
+    .then((project) => {res.send(project)});
+})
+
+
+// table.increments('id');
+// table.string('name');
+// table.string('problem_statement');
+// table.integer('submitter_id');
+// table.foreign('submitter_id').references('user_table.id');
+// table.boolean('is_approved');
+// table.boolean('is_accepted');
+// table.integer('accepted_by_id');
+// table.foreign('accepted_by_id').references('user_table.id');
+// table.boolean('is_completed');
+
+
+app.post('/projects', (req, res) => {
+  knex("user_table")
+  .insert(req.body)
+  .then((newProject) => {
+    res.send(
+      req.body.name,
+      req.body.problem_statement,
+      req.body.submitter,
+      req.body.is_approved,
+      req.body.is_accepted,
+      req.body.accepted_by_id,
+      req.body.is_completed,
+      `post was successful`
+    );
+      console.log('post was successful')
+  })
+})
+
+app.patch('/projects/:id', (req, res) => {
+  knex('project_table')
+    .where({ id: req.params.id })
+    .update({
+      name: req.body.name,
+      problem_statement: req.body.problem_statement,
+      submitter: req.body.submitter,
+      is_approved: req.body.is_approved,
+      is_accepted: req.body.is_accepted,
+      accepted_by_id: req.body.accepted_by_id,
+      is_completed: req.body.is_completed,
+    })
+    .then((updateRows) => res.send('project updated'))
+})
+
+app.delete('/projects/:id', (req, res) => {
+  knex('project_table')
+    .where({ id: req.params.id })
+    .del()
+    .then(res.send('project deleted'))
+})
 
 app.listen(port, () => console.log(`Express server listening in on port ${port}`))
 
