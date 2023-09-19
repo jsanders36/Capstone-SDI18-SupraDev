eab05f28f753ae8758baa6e6592c6cccbc0ac53d
Connor Smith <cmsmith1217@gmail.com>
2023-09-19 16:04:18 -0500

    fixed cookies for navbar, cond rendering for profilepage button on navbar, user profile page can pull data from specific user and projects apis

--- ui/src/Navbar.js
+++ ui/src/Navbar.js
@@ -6,15 +6,20 @@ import { Button, IconButton } from '@mui/material';
 import { useCookies, CookiesProvider } from 'react-cookie';
 
 const Navbar = () => {
-    const [sessionCookies, setSessionCookies, removeSessionCookies] = useCookies(['username_token', 'user_id_token'])
-    console.log('cookies:', sessionCookies)
+    const [sessionCookies, setSessionCookies, removeSessionCookies] = useCookies(['username_token', 'user_id_token', 'userPriv_Token'])
 
     let logoutButton;
     let currentUserInfo;
+    let profileButton;
 
     if (sessionCookies.username_token) {
         logoutButton = <Button as={Link} onClick={() => { removeSessionCookies('username_token'); removeSessionCookies('user_id_token'); alert('You have been logged out') }} to='/login' variant='contained' color='error' style={{ gap: '10px', margin: '10px' }}>Logout</Button>;
         currentUserInfo = <p style={{ marginLeft: '20%', outlineStyle: 'solid', outlineColor: 'black', outlineWidth: '1px', backgroundColor: 'rgb(255, 0, 255)' }}> Current User: {`${sessionCookies.username_token} `} </p>
+        if (sessionCookies.userPriv_Token === true) {
+            profileButton = <Button as={Link} to='/user-profile' variant='contained' color='secondary' style={{ gap: '10px', margin: '10px' }}>Admin Page</Button>
+        } else {
+            profileButton = <Button as={Link} to='/user-profile' variant='contained' color='secondary' style={{ gap: '10px', margin: '10px' }}>User Page</Button>
+        }
     }
 
     return (
@@ -23,7 +28,7 @@ const Navbar = () => {
             <Button as={Link} to='/projects' variant='contained' color='secondary' style={{ gap: '10px', margin: '10px' }}>Projects</Button>
             <Button as={Link} to='/requests' variant='contained' color='secondary' style={{ gap: '10px', margin: '10px' }}>Requests</Button>
             <Button as={Link} to='/login' variant='contained' color='secondary' style={{ gap: '10px', margin: '10px' }}>Login Page</Button>
-            <Button as={Link} to='/user-profile' variant='contained' color='secondary' style={{ gap: '10px', margin: '10px' }}>Admin Page</Button>
+            {profileButton}
             {logoutButton}
             {currentUserInfo}
         </div>
