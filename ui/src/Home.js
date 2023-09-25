46258c488019943c9d3ecd0b9ec298870c0a3903
jsanders36 <jsaf36@gmail.com>
2023-09-25 16:23:06 -0600

    Software News

--- ui/src/Home.js
+++ ui/src/Home.js
@@ -27,15 +27,43 @@ const HoverCard = styled(motion(Card))(({ theme }) => ({
 const HomePage = () => {
     const theme = useTheme();
 
-    const [spaceSoftware, setSpaceSoftware] = useState([]);
 
-    useEffect(() => {
-        // Fetch space-related software data from NASA's TechTransfer Software API
-        fetch('https://api.nasa.gov/techtransfer/software/?engine&api_key=6WY9lR6IeiiTvLJG4U5V4qnnJzPJlpgMkmV8uKj9')
-            .then(response => response.json())
-            .then(data => setSpaceSoftware(data.results.slice(0, 10))) // Limit to 10 items
-            .catch(error => console.error('Error fetching space software data:', error));
-    }, []);
+
+    const spaceSoftware = [
+        {
+            title: "SSC Data-Management Software Plays Critical Role in SDA, Afghanistan Airlift",
+            description: "Sophisticated data management and analytic software programs are not only enhancing Space Systems Command’s (SSC) ability to carry out its Space Domain Awareness mission, but also have been leveraged to provide a rapid response to humanitarian crises across the globe. \"Data is the life blood of our digital force and our commercial partners have a lot that they can offer,” said Col. Jennifer M. Krolikowski, senior materiel leader for SSC Enterprise Corps’ West Coast data/cyber coding factory serving the United States Space Force (USSF). “These commercial solutions allow us to go faster which ultimately helps us turn that data into knowledge to drive decision-making.\”",
+            link: "https://www.spaceforce.mil/News/Article-Display/Article/2813059/ssc-data-management-software-plays-critical-role-in-sda-afghanistan-airlift/"
+        },
+        {
+            title: "The ever-evolving digital war fighter",
+            description: "Supra coders are Airmen and Guardians who develop, manage, and design software for the U.S. Space Force. These individuals serve in a variety of specialties. Once they complete the Software Development Immersive (SDI) class, a software development boot camp that teaches full-stack JavaScript development and application deployment, they return to their bases to begin developing applications and solutions. U.S. Air Force Tech. Sgt. Urich Garcia, 45th Security Forces Squadron supra coder, and U.S. Air Force Staff Sgt. Brian Hardy, 45th SFS supra coder, completed the SDI class December 3, 2021 making them the first supra coders at Patrick SFB and the first supra coders in the security forces career field.",
+            link: "https://www.spaceforce.mil/News/Article-Display/Article/3029674/the-ever-evolving-digital-war-fighter/"
+        },
+        {
+            title: "Digital University: Enabling a force for the future",
+            description: "Maintaining the U.S. Space Force’s strategic advantage in the space domain requires cutting-edge technology, but it also takes a workforce that can effectively and efficiently leverage that technology. To develop its force for the future, the Space Force is working with industry to provide educational resources in topics like Data Science, Artificial Intelligence, Software Development, Product Management, Design, Cybersecurity and Cloud Architecture through its Digital University.",
+            link: "https://www.spaceforce.mil/News/Article/2926515/digital-university-enabling-a-force-for-the-future/"
+        },
+        {
+            title: "Space Cockpit a new way to visualize space operations",
+            description: "Space Cockpit is a situational awareness tool that allows satellite operators to visualize the satellites they control in a real-time, video game-like application. Originally commercial software, 1st Lieutenants Tory Smith and Jacqueline Cromer, Space Commercially Augmented Mission Platform (Space CAMP) software development leads, along with their product development team, spent months developing the software for Space Force professional’s use.",
+            link: "https://www.spaceforce.mil/News/Article/2434500/space-cockpit-a-new-way-to-visualize-space-operations/"
+        },
+        {
+            title: "392d CTS completes its first USEUCOM-focused SPACE FLAG exercise",
+            description: "...“For the first time in SPACE FLAG, Cyber Guardians and Super Coders planned and executed simulated combat operations in real-time throughout the entirety of the exercise,” said U.S. Space Force Capt. Karl Pruhsmeier, 392d CTS and SPACE FLAG 23-1 exercise director. “Using a U.S. Air Force cyber range, the 527th Space Aggressor squadron was able to help us train multi-domain mission planning and execution, which exposed numerous opportunities to enhance mission assurance for orbital warfare, space domain awareness, space battle management, and other space warfighting functions.”",
+            link: "https://www.spaceforce.mil/News/Article-Display/Article/3251072/392d-cts-completes-its-first-useucom-focused-space-flag-exercise/"
+        }
+
+    ]
+    // useEffect(() => {
+    //     // Fetch space-related software data from NASA's TechTransfer Software API
+    //     fetch('https://api.nasa.gov/techtransfer/software/?engine&api_key=6WY9lR6IeiiTvLJG4U5V4qnnJzPJlpgMkmV8uKj9')
+    //         .then(response => response.json())
+    //         .then(data => setSpaceSoftware(data.results.slice(0, 10))) // Limit to 10 items
+    //         .catch(error => console.error('Error fetching space software data:', error));
+    // }, []);
 
     return (
         <Box
@@ -83,14 +111,14 @@ const HomePage = () => {
                             <CardHeader title="Space Software News" titleTypographyProps={{ variant: 'h5', fontWeight: 'bold' }} />
                             <CardContent>
                                 {spaceSoftware.map((softwareItem, index) => (
-                                    <div key={index}>
-                                        <Typography variant="h6" color="primary">
-                                            {softwareItem[2].slice(0, 100)} {/* Limit to 50 characters */}
-                                        </Typography>
-                                        <Typography variant="body1" mt={1}>
-                                            {softwareItem[3].slice(0, 250)}... {/* Limit to 200 characters */}
-                                        </Typography>
-                                    </div>
+                                   <div key={index}>
+                                   <Typography variant="h6" color="primary">
+                                     <a href={softwareItem.link} target="_blank" rel="noopener noreferrer">{softwareItem.title.slice(0, 100)}</a>
+                                   </Typography>
+                                   <Typography variant="body1" mt={1}>
+                                     {softwareItem.description.slice(0, 250)}...
+                                   </Typography>
+                                 </div>
                                 ))}
 
                             </CardContent>
